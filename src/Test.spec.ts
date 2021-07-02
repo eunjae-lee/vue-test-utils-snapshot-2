cc036427dfc11c32f353f462de0c0f2c13252cb4
eunjae-lee <karis612@gmail.com>
2021-07-02 18:04:51 +0200

    .

--- src/Test.spec.ts
+++ src/Test.spec.ts
@@ -7,3 +7,31 @@ test("uses mounts", async () => {
     `"<div><button class=\\"hello\\" title=\\"this is a button\\">click</button></div>"`
   );
 });
+
+test("lifecycle methods in child component", () => {
+  const Parent = {
+    template: `
+      <div>
+        <p>I am a parent</p>
+        <slot />
+      </div>
+    `,
+  };
+  const Child = {
+    created() {
+      throw new Error("error?");
+      console.log("child created");
+    },
+    mounted() {
+      console.log("child mounted");
+    },
+    render() {
+      return "i am a child";
+    },
+  };
+
+  const wrapper = mount(Parent, {
+    slots: { default: Child },
+  });
+  console.log(wrapper.html());
+});
